23c98d9396e73d31de2a52f5594ef09def4da1aa
李育佶 <Liyugee@github.com>
2018-04-19 17:55:25 +0800

    1、添加头像相关UI

---
+++ doudizhu_client/assets/scripts/gameScene/prefab/playerNode.js
@@ -0,0 +1,21 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        headImage: cc.Sprite,
+        idLabel: cc.Label,
+        nickNameLabel: cc.Label,
+        goldLabel: cc.Label
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
